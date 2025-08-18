d1d46acdbb6089e1fb79f8030ba644740b01ff34
PNavrotskyi <pavlo.navrotskyi@cqlcorp.com>
2025-08-18 14:59:43 +0300

    feat: add GetEstimateDto and implement estimate retrieval in reports service and controller

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,21 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { NextFunction, Request, Response } from 'express';
+import { User } from '../user.entity';
+
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       currentUser?: User;
+//     }
+//   }
+// }
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    currentUser?: User;
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -11,8 +26,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.usersService.findById(userId);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       req.currentUser = user;
     }
 
