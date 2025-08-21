0fff83b5a3b4327218ecd55d3da45f316f50c855
PNavrotskyi <pavlo.navrotskyi@cqlcorp.com>
2025-08-21 12:53:32 +0300

    fix: update import paths for consistency across reports and users modules

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
 
